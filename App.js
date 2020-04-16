bcf08777fb48491dc38ec2ec138324255fc6b32a
Alexander Jacks <alexanderthejacks@gmail.com>
2020-04-16 12:54:54 -0700

    :wrench: test adjustment

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
+        <Text style={styles.welcome}>Welcome to 2020 Coding Quarantiner!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
         {!firebase.apps.length && (
